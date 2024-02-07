38ad44fd479264c0a162c740f2fe78a4482a9acf
Dan Duguay <Duguay9@gmail.com>
2024-02-07 18:02:37 -0500

    Still working on E2E tests

--- client/cypress/e2e/spec.cy.js
+++ client/cypress/e2e/spec.cy.js
@@ -2,6 +2,7 @@
 
 beforeEach(()=> {
   cy.visit('http://localhost:5173/')
+  cy.wait(4000)
 })
 
 describe("VisitHomepage", ()=> {
@@ -13,6 +14,7 @@ describe("VisitHomepage", ()=> {
 describe("NavigateToGoogleSignIn", () => {
   it("should Navigate to a google sign in page when 'Sign in With Google' is clicked", () => {
     cy.contains("button", "Sign in With Google").click()
+    cy.wait(4000)
     cy.location("pathname").should('eq', "/googleSignin")
   })
 })
@@ -20,8 +22,10 @@ describe("NavigateToGoogleSignIn", () => {
 describe("NewUserSignIn-Email", () => {
   it("Should let new sign up with a username, name, email, and password", () => {
     cy.get(".login__content").contains("a","New user, sign up!").click()
+    cy.wait(4000)
     cy.location("pathname").should('eq',"/SignUp")
     cy.get(".email__signUp").contains("a","Sign up with email").click()
+    cy.wait(4000)
     cy.location("pathname").should('eq',"/SignUp/userSignUp")
     cy.get("input[name='username']").type("CypressTestUsername").should("have.value",
         "CypressTestUsername")
@@ -32,6 +36,7 @@ describe("NewUserSignIn-Email", () => {
     cy.get("input[name='password']").type("CypressTestPassword").should("have.value",
         "CypressTestPassword")
     cy.get(".continue__button").click()
+    cy.wait(4000)
     cy.location("pathname").should('eq', "/")
   })
 })
@@ -43,6 +48,7 @@ describe("PublicUserSignIn", () => {
     cy.get("input[name='password']").type("CypressTestPassword").should("have.value",
         "CypressTestPassword")
     cy.get(".continue__button").click()
+    cy.wait(4000)
     cy.location("pathname").should('eq', "/DashboardHome")
   })
 })
