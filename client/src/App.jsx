e595dfdf1baf940e83b39761fc4f7065df228964
Houssam Righi <hrighi01@gmail.com>
2024-02-17 15:40:52 -0500

    Fixed minor issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,25 @@
-import React, { useState, useEffect} from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LoginPage from './Components/Login/LoginPage'
-import SignUpPage from './Components/SignUp/SignUpPage'
-import './App.css'
-import UserSignUp from './Components/SignUp/UserSignUp'
-import DashboardHome from './Components/Dashboard/DashboardHome'
-import EditAccount from './Components/EditAccount/EditAccount';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./Components/Login/LoginPage";
+import SignUpPage from "./Components/SignUp/SignUpPage";
+import "./App.css";
+import UserSignUp from "./Components/SignUp/UserSignUp";
+import DashboardHome from "./Components/Dashboard/DashboardHome";
+import EditAccount from "./Components/EditAccount/EditAccount";
 
 function App() {
-
-
   return (
     <>
-       <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LoginPage />}/>
-                <Route path="/SignUp" element={<SignUpPage/>}/>
-                <Route path="/SignUp/userSignUp" element={<UserSignUp/>}/>
-                <Route path="/DashboardHome" element ={<DashboardHome/>}/>
-                <Route path="/DashboardHome/editUser" element ={<EditAccount/>}/>
-
-            </Routes> 
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/SignUp" element={<SignUpPage />} />
+          <Route path="/SignUp/userSignUp" element={<UserSignUp />} />
+          <Route path="/DashboardHome" element={<DashboardHome />} />
+          <Route path="/DashboardHome/editUser" element={<EditAccount />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/Components/Dashboard/DashboardHome.jsx
+++ client/src/Components/Dashboard/DashboardHome.jsx
@@ -1,85 +1,77 @@
-import React, {useState, useEffect} from "react";
-import { Link } from "react-router-dom";
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
-import './DashboardHome.css'
+import "./DashboardHome.css";
 import SideDrawer from "./SideDrawer";
-// Firebase imports
-import {db} from '../../../../firebase/firebaseConfig'
-import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from "firebase/firestore"
 import { LuConstruction } from "react-icons/lu";
 
 // Dashboard home is the home component where clients will enter
 // It will host the side drawer, profile information, condo information all that
-const DashBoardHome = props => {
-
-// toggles the drawer between being open and closed
-const [isDrawerOpen, setDrawerOpen] = useState(false);
+const DashBoardHome = () => {
+  // toggles the drawer between being open and closed
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
 
   const toggleDrawer = () => {
     setDrawerOpen(!isDrawerOpen);
   };
 
   const options = [
-    {key:1, option: 'Label 1'},
-    {key:2, option: 'Label 2'},
-    {key:3, option: 'Label 3'},
-    {key:4, option: 'Label 4'},
-  ]
+    { key: 1, option: "Label 1" },
+    { key: 2, option: "Label 2" },
+    { key: 3, option: "Label 3" },
+    { key: 4, option: "Label 4" },
+  ];
 
-  const dummyUser ={
-    fName: 'Condo',
-    lName: 'Owner'
-  }
+  const dummyUser = {
+    fName: "Condo",
+    lName: "Owner",
+  };
 
-  const [dots,setDots] = useState('.');
+  const [dots, setDots] = useState(".");
 
   setTimeout(() => {
-      if(dots === '.'){
-        setDots('..')
-      }
-      else if(dots === '..'){
-        setDots('...')
-      }
-      else{
-       setDots('.')
-      }
-  }, 1000)
-
-
+    if (dots === ".") {
+      setDots("..");
+    } else if (dots === "..") {
+      setDots("...");
+    } else {
+      setDots(".");
+    }
+  }, 1000);
 
   return (
     <div className="dashboard__home">
-        <div className="sidedrawer__open">
-           
-        </div>
-        <button className="patty__button" onClick={toggleDrawer}><RxHamburgerMenu size={40}/></button>
+      <div className="sidedrawer__open"></div>
+      <button className="patty__button" onClick={toggleDrawer}>
+        <RxHamburgerMenu size={40} />
+      </button>
 
-        {/* The Side drawer is whats being opened for main navigation */}
-      <SideDrawer isOpen={isDrawerOpen} onClose={toggleDrawer} firstName={dummyUser.fName} lastName={dummyUser.lName}>
+      {/* The Side drawer is whats being opened for main navigation */}
+      <SideDrawer
+        isOpen={isDrawerOpen}
+        onClose={toggleDrawer}
+        firstName={dummyUser.fName}
+        lastName={dummyUser.lName}
+      >
         <div className="link__holder">
-           {options && (
-                options.map(obj => {
-                    return(
-                        <div key={obj.key}className="link__option">
-                            <p>{obj.option}</p>
-                        </div>
-                    )
-                })
-           )}
-           </div>
+          {options &&
+            options.map((obj) => {
+              return (
+                <div key={obj.key} className="link__option">
+                  <p>{obj.option}</p>
+                </div>
+              );
+            })}
+        </div>
       </SideDrawer>
       {/* Your main content goes here */}
-     <div className="container">
-     <div className="construction__sign">
-                  <LuConstruction size={60}/>
-                  <h1>Site in construction {dots}</h1>
-              </div>
-     </div>
-              
+      <div className="container">
+        <div className="construction__sign">
+          <LuConstruction size={60} />
+          <h1>Site in construction {dots}</h1>
+        </div>
+      </div>
     </div>
-  
-    )
-}
+  );
+};
 
 export default DashBoardHome;
