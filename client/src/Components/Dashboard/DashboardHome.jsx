c5fab474741a4e7ec940dbd13e2721f78483a96b
Walid <walidoo_2002@hotmail.com>
2024-02-27 20:15:56 -0500

    feat: add table header card for view properties in dashboard

--- client/src/Components/Dashboard/DashboardHome.jsx
+++ client/src/Components/Dashboard/DashboardHome.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
-import { RxHamburgerMenu } from "react-icons/rx";
+import {useState} from "react";
+import {RxHamburgerMenu} from "react-icons/rx";
 import "./DashBoardHome.css";
 import SideDrawer from "./SideDrawer";
-import { LuConstruction } from "react-icons/lu";
+import TableCard from "../Cards/Tables/TableCard.jsx";
+import TableCardHeader from "../Cards/Tables/TableCardHeader.jsx";
+import {Link} from "react-router-dom";
+import AddButton from "../Buttons/AddButton.jsx";
 
 // Dashboard home is the home component where clients will enter
 // It will host the side drawer, profile information, condo information all that
@@ -15,10 +18,10 @@ const DashBoardHome = () => {
   };
 
   const options = [
-    { key: 1, option: "Label 1" },
-    { key: 2, option: "Label 2" },
-    { key: 3, option: "Label 3" },
-    { key: 4, option: "Label 4" },
+    {key: 1, option: "Label 1"},
+    {key: 2, option: "Label 2"},
+    {key: 3, option: "Label 3"},
+    {key: 4, option: "Label 4"},
   ];
 
   const dummyUser = {
@@ -26,23 +29,12 @@ const DashBoardHome = () => {
     lName: "Owner",
   };
 
-  const [dots, setDots] = useState(".");
-
-  setTimeout(() => {
-    if (dots === ".") {
-      setDots("..");
-    } else if (dots === "..") {
-      setDots("...");
-    } else {
-      setDots(".");
-    }
-  }, 1000);
 
   return (
     <div className="dashboard__home">
       <div className="sidedrawer__open"></div>
       <button className="patty__button" onClick={toggleDrawer}>
-        <RxHamburgerMenu size={40} />
+        <RxHamburgerMenu size={40}/>
       </button>
 
       {/* The Side drawer is whats being opened for main navigation */}
@@ -64,10 +56,17 @@ const DashBoardHome = () => {
         </div>
       </SideDrawer>
       {/* Your main content goes here */}
-      <div className="container flex flex-col items-center">
-        <div className="flex flex-col justify-center items-center">
-          <LuConstruction size={60} />
-          <h1>Site in construction {dots}</h1>
+      <div className="container flex flex-col items-center px-4">
+        <div className="flex flex-col justify-center items-center w-full">
+          {/* Properties card goes here */}
+          <TableCard>
+            <TableCardHeader title={"My Properties 🏢"}>
+              <div className="flex items-center gap-4">
+                <Link className="underline" to={""}>See more</Link>
+                <AddButton>Add Property</AddButton>
+              </div>
+            </TableCardHeader>
+          </TableCard>
         </div>
       </div>
     </div>
