bc02af34d377a9167ed376d2e97684a6cbed0cea
Walid <walidoo_2002@hotmail.com>
2024-02-27 22:23:44 -0500

    feat: add className prop to TableCard component

--- client/src/Components/Cards/Tables/TableCard.jsx
+++ client/src/Components/Cards/Tables/TableCard.jsx
@@ -2,11 +2,15 @@ import PropTypes from "prop-types";
 
 export default function TableCard(props) {
   return (
-    <div className="flex flex-col justify-between w-full h-fit p-4 bg-white rounded border border-gray-400 shadow-md">
+    <div className={
+      `flex flex-col justify-between w-full h-fit p-8 bg-white rounded-[10px] border border-gray-400 
+      shadow-xl ${props.className}`}
+    >
       {props.children}
     </div>)
 }
 
 TableCard.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 }
