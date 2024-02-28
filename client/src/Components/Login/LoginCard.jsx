6f40b785667fabfb79f3679b7abe9b7e332fc224
Houssam Righi <hrighi01@gmail.com>
2024-02-28 18:24:11 -0500

    Add jwt-decode dependency and fix login functionality

--- client/src/Components/Login/LoginCard.jsx
+++ client/src/Components/Login/LoginCard.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
-import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 import axios from "axios";
 
 import "./LoginCard.css";
 import ContinueButton from "../Buttons/ContinueButton";
-
 const LoginCard = () => {
   const navigation = useNavigate();
 
@@ -16,45 +14,34 @@ const LoginCard = () => {
     email: "",
     password: "",
   });
+  const [incorrectInfo, setIncorrectInfo] = useState(false);
+  const [error, setError] = useState(false);
 
   // On click of button, this will login the users and redirect them to their profiles
   const handleClick = async (e) => {
     e.preventDefault();
-
     // This is where the user will be logged in and redirected to their profile
     axios
-      .post("/api/login", userInfo)
+      .post("http://localhost:3000/api/v1/login", userInfo) // Added 'http://' protocol
       .then((res) => {
-        if (res.status !== 200) {
-          console.log("Issue logging in");
+        if (res.data.token) {
+          console.log("Logged in successfully");
+          let userData = jwtDecode(res.data.token);
+          console.log("User data:", userData);
+          // navigation("/DashboardHome");
         } else {
-          console.log("Successfully logged in");
+          console.log("Incorrect email or password");
+          setIncorrectInfo(true);
         }
       })
       .catch((err) => {
-        console.log("Error logging in:", err.response.data.message);
+        console.log("Error logging in:", err);
+        setError(true);
       });
-
-    // This is where the user will be logged in and redirected to their profile
-    axios
-      .post("/api/login", userInfo)
-      .then((res) => {
-        if (res.status !== 200) {
-          console.log("Issue logging in");
-        } else {
-          console.log("Successfully logged in");
-        }
-      })
-      .catch((err) => {
-        console.log("Error logging in:", err.response.data.message);
-      });
-
-    navigation("/DashboardHome");
   };
   // Function that stores the users information into the object for querying
   const handleChange = (e) => {
     setUserInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-    console.log(userInfo);
   };
 
   return (
@@ -72,43 +59,12 @@ const LoginCard = () => {
 
           <p> Or sign in with email</p>
 
-          <div className="flex flex-col gap-6 justify-center text-center">
-            <Link
-              to="/googleSignin"
-              className="flex gap-4 py-2 w-full bg-[#F0F1F5] rounded border-grey-300 border items-center justify-center"
-            >
-              <FcGoogle size={25} />
-              <p className="text font-semibold">Sign in With Google</p>
-            </Link>
+          {incorrectInfo && (
+            <p className="text-red-500">Incorrect email or password</p>
+          )}
 
-            <p> Or sign in with email</p>
-
-            <div>
-              <div className="credentials__section c1">
-                <input
-                  type="email"
-                  placeholder="Email"
-                  name="email"
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="credentials__section c2">
-                <input
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="row">
-                <div className="col-lg-6 col-sm-3 signup__redirect">
-                  <Link to="/SignUp">New user, sign up!</Link>
-                </div>
-                <div className="col-lg-6 col-sm-3 forgot__password">
-                  <Link to="/">Forgot Password?</Link>
-                </div>
-              </div>
-            </div>
+          {error && <p className="text-red-500">Internal Server Error</p>}
+          <div>
             <div className="credentials__section c1">
               <input
                 type="email"
@@ -134,8 +90,8 @@ const LoginCard = () => {
               </div>
             </div>
           </div>
-          <ContinueButton onClick={handleClick} name={"Sign In"} />
         </div>
+        <ContinueButton onClick={handleClick} name={"Sign In"} />
       </div>
     </div>
   );
--- client/src/Components/Login/LoginPage.jsx
+++ client/src/Components/Login/LoginPage.jsx
@@ -1,12 +1,11 @@
 import "./LoginPage.css";
-import { v4 as uuidv4 } from "uuid";
+
 import { useMediaQuery } from "react-responsive";
 import LoginCard from "./LoginCard";
 import SidePicture from "./SidePicture";
 
 const LoginPage = () => {
   const isMobile = useMediaQuery({ query: `(max-width: 760px)` }); // Hook that verifies if display is media or desktop
-  console.log(uuidv4());
   console.log(isMobile);
   return (
     <div className="login__main__page">
