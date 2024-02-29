167d7188347d0c3ed06399d2f8aac8f9834d89ee
Mohammed Afif Rahman <99463967+Afifr2001@users.noreply.github.com>
2024-02-28 22:43:50 -0500

    Added Update, Delete Property Methods Backend

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const CondoOwnerRoutes = require('./src/CondoOwner/routes')
 const CondoRenterRoutes = require('./src/CondoRenter/routes')
 const CMCRoutes = require('./src/CMC/routes')
 const PublicUserRoutes = require('./src/PublicUser/routes')
+const PropertyRoutes = require('./src/Property/routes')
 const Login = require('./src/Login/routes')
 const PropertyProfileRoutes = require('./src/Property/routes')
 const cors = require('cors');
@@ -22,6 +23,6 @@ app.use('/api/v1/cr', CondoRenterRoutes);
 app.use('/api/v1/cmc', CMCRoutes);
 app.use('/api/v1/pu', PublicUserRoutes);
 app.use('/api/v1/login', Login);
-app.use("/api/v1/pp", PropertyProfileRoutes);
+app.use('/api/v1/pp', PropertyRoutes);
 
 app.listen(port, () => console.log(`app listening on ${port}`));
