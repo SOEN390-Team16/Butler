4743a481e22158f0733ca1e9bc641a69bfa7f525
Houssam Righi <hrighi01@gmail.com>
2024-03-01 21:48:09 -0500

    Refactor ContinueButton component

--- client/src/Components/Buttons/ContinueButton.jsx
+++ client/src/Components/Buttons/ContinueButton.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { Link} from "react-router-dom";
-import './ContinueButton.css'
+import { Link } from "react-router-dom";
+import "./ContinueButton.css";
 
+const ContinueButton = (props) => {
+  // Continue button is the button that is used for logging in and signing up for accounts
 
-const ContinueButton = props => {
-
-   // Continue button is the button that is used for logging in and signing up for accounts
-
-    return(
+  return (
     <div className="continue__button">
-        <Link onClick={props.onClick} ><button>{props.name}</button></Link>
+      <button onClick={props.onClick}>{props.name}</button>
     </div>
-    )
-}
+  );
+};
 
 export default ContinueButton;
