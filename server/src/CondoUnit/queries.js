3b32685cb8e9313c4443cc2afd2cc0ff9a876460
ceyhuntopcu <ceyhuntopcu4107@gmail.com>
2024-03-17 13:42:13 -0400

    Update condo_unit queries

--- server/src/CondoUnit/queries.js
+++ server/src/CondoUnit/queries.js
@@ -1,14 +1,14 @@
 const getCondoUnits = "SELECT * FROM condo_unit";
-const getCondoUnitById = "SELECT cu.condoid, cu.lockerid, cu.parkingid, cu.companyid, cu.propertyid, cu.condo_size, cu.condo_fee, cu.condo_number FROM condo_unit cu WHERE cu.condoid = $1";
-const checkIfCondoUnitExistsById = "SELECT * FROM condo_unit cu WHERE cu.condoid = $1";
-const addCondoUnit = "INSERT INTO condo_unit (condoid, lockerid, parkingid, companyid, propertyid, condo_size, condo_fee, condo_number) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
-const removeCondoUnit  = "DELETE FROM condo_unit condoid = $1";
-const updateCondoUnit = "UPDATE condo_unit SET lockerid = $1, parkingid = $2, companyid = $3, propertyid = $4, condo_size = $5, condo_fee = $6, condo_number = $7 WHERE condoid = $8";
+const getCondoUnitById = "SELECT cu.condoid, cu.lockerid, cu.parkingid, cu.companyid, cu.propertyid, cu.condo_size, cu.condo_fee, cu.condo_number, cu.condofee_persqrft FROM condo_unit cu WHERE cu.condoid = $1";
+const checkIfCondoUnitExists = "SELECT * FROM condo_unit cu WHERE cu.condoid = $1";
+const addCondoUnit = "INSERT INTO condo_unit (condoid, lockerid, parkingid, companyid, propertyid, condo_size, condo_fee, condo_number, condofee_persqrft) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
+const removeCondoUnit  = "DELETE FROM condo_unit WHERE condoid = $1";
+const updateCondoUnit = "UPDATE condo_unit SET lockerid = $1, parkingid = $2, companyid = $3, propertyid = $4, condo_size = $5, condo_fee = $6, condo_number = $7, condofee_persqrft = $8 WHERE condoid = $9";
 
 module.exports = {
     getCondoUnits,
     getCondoUnitById,
-    checkIfCondoUnitExistsById,
+    checkIfCondoUnitExists,
     addCondoUnit,   
     removeCondoUnit,
     updateCondoUnit
