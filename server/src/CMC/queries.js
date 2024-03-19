fa698f46bd7dab9e3a83165fdbb1592ce7015fd6
DanDuguay <Duguay9@gmail.com>
2024-03-18 21:28:25 -0400

    Making the linter happy with the dev branch

--- server/src/CMC/queries.js
+++ server/src/CMC/queries.js
@@ -1,13 +1,13 @@
-const getCMCs = "SELECT companyID, company_name, email, password FROM  condo_management_company;";
-const getCMCById = "SELECT companyID, company_name, email, password FROM condo_management_company WHERE companyID = $1;"; 
-const checkIfCMCEmailExists = "SELECT * FROM condo_management_company cmc WHERE cmc.email = $1;";
-const addCMC = "INSERT INTO condo_management_company(company_name, email, password) VALUES ($1, $2, $3);";
-const removeCMC = "DELETE FROM condo_management_company WHERE companyID = $1;";
+const getCMCs = 'SELECT companyID, company_name, email, password FROM  condo_management_company;'
+const getCMCById = 'SELECT companyID, company_name, email, password FROM condo_management_company WHERE companyID = $1;'
+const checkIfCMCEmailExists = 'SELECT * FROM condo_management_company cmc WHERE cmc.email = $1;'
+const addCMC = 'INSERT INTO condo_management_company(company_name, email, password) VALUES ($1, $2, $3);'
+const removeCMC = 'DELETE FROM condo_management_company WHERE companyID = $1;'
 
 module.exports = {
-    getCMCs,
-    getCMCById,
-    checkIfCMCEmailExists,
-    addCMC,
-    removeCMC
-};
+  getCMCs,
+  getCMCById,
+  checkIfCMCEmailExists,
+  addCMC,
+  removeCMC
+}
