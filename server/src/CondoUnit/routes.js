322c3fa0c8c3bfbcb89d63ba00b25aff6c436bdb
wade3hamati <wade3_hamati@outlook.com>
2024-03-18 23:31:09 -0400

    fix merge conflicts

--- server/src/CondoUnit/routes.js
+++ server/src/CondoUnit/routes.js
@@ -7,8 +7,5 @@ router.get('/', authenticateToken, controller.getAllUnits)
 router.get('/:condoid', authenticateToken, controller.getCondoUnitById)
 router.get('/getByP/:property_id', authenticateToken, controller.getCondoUnitsByPropertyId)
 router.get('/getByC/:companyid', authenticateToken, controller.getCondoUnitsByCompanyId)
-router.post('/', authenticateToken, controller.addCondoUnit)
-router.patch('/:condoid', authenticateToken, controller.updateCondoUnit)
-router.delete('/:condoid', authenticateToken, controller.removeCondoUnit)
 
 module.exports = router
