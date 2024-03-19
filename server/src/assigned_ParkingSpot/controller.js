54862c7a15f90b37181d5ed8c74d186e2881f23f
DanDuguay <Duguay9@gmail.com>
2024-03-19 17:57:27 -0400

    fixed errors mentioned in review.

--- server/src/assigned_ParkingSpot/controller.js
+++ server/src/assigned_ParkingSpot/controller.js
@@ -2,7 +2,7 @@ const pool = require('../../db')
 const queries = require('./queries')
 const assignParkingSpotByUserId = (req, res) => {
   console.log('Assigning a parking spot')
-  const { userid } = req.body
+  const userid = parseInt(req.params.userid)
   pool.query(queries.assignParkingSpotByUserId, [userid], (error, results) => {
     if (error) {
       console.error('Error assigning parking spot: ', error)
@@ -32,8 +32,6 @@ const getAssignedParkingSpots = (req, res) => {
     if (error) {
       console.error('Error getting assigned parking spots: ', error)
       res.status(500).json({ error: 'Internal Server Error' })
-    } else if (results.rows.length === 0) {
-      res.status(404).json({ error: 'Assigned Parking Spots Not Found' })
     } else {
       res.status(200).json(results.rows)
     }
@@ -42,19 +40,16 @@ const getAssignedParkingSpots = (req, res) => {
 
 const getAssignedParkingSpotByUserId = (req, res) => {
   console.log('Getting Assigned Parking Spot By Userid')
-  const userid = req.body
+  const userid = parseInt(req.params.userid)
   pool.query(queries.getAssignedParkingSpotByUserId, [userid], (error, results) => {
   if (error) {
     console.error('Error getting assigned parking spot by user id: ', error)
     res.status(500).json({ error: 'Internal Server Error' })
-  } else if (results.rows.length === 0) {
-    res.status(404).json({ error: 'Assigned Parking Spot Not Found' })
   } else {
     res.status(200).json(results.rows)
   }
 })
 }
-
 
 module.exports = {
   assignParkingSpotByUserId,
