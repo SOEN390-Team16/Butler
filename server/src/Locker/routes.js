6fd08fd659449fd5cd6ce8cdee15cca65e6cb76b
DanDuguay <Duguay9@gmail.com>
2024-03-19 02:01:25 -0400

    fixed issues with locker routes

--- server/src/Locker/routes.js
+++ server/src/Locker/routes.js
@@ -7,7 +7,5 @@ router.get('/', authenticateToken, controller.getAllLockers)
 router.get('/getByL/:lockerid', authenticateToken, controller.getLockerById)
 router.get('/getByP/:propertyid', authenticateToken, controller.getLockersByPropertyId)
 router.get('/getByC/:companyid', authenticateToken, controller.getLockersByCompanyId)
-router.post('/', authenticateToken, controller.addLocker)
-router.delete('/:lockerid', authenticateToken, controller.removeLockerByLockerId)
 
 module.exports = router
