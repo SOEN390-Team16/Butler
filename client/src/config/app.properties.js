dc45b5facd76fdc9b10b5c1aaab4cb2454bb4504
Walid <walidoo_2002@hotmail.com>
2024-04-06 18:04:41 -0400

    implemented zustand as the container state management tool for login

---
+++ client/src/config/app.properties.js
@@ -0,0 +1,3 @@
+export class AppProperties {
+  static API_URL = "http://hortzcloud.com:3000";
+}
---
+++ client/src/store/auth/auth.store.js
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+import AuthService from "../../service/auth/auth.service.js";
+import { toast } from "react-toastify";
+
+const TOKEN = "token"
+
+const useAuthStore = create((set, get) => ({
+  setToken: (token) => {
+    localStorage.setItem(TOKEN, token);
+  },
+  clearToken: () => {
+    localStorage.removeItem(TOKEN);
+  },
+  login: async (email, password) => {
+    try {
+      const res = await AuthService.login(email, password);
+      if (res.data && res.data.token) {
+        get().setToken(res.data.token);
+        return res.data;
+      }
+    } catch (err) {
+      toast.error("Login Failed: " + ("Unknown error"));
+    }
+
+  },
+
+}))
+
+export default useAuthStore
