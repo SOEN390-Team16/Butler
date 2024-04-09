997476f67b62b6f795e19e84500f44225f8d78f3
Walid <walidoo_2002@hotmail.com>
2024-04-09 00:57:05 -0400

    feat(client): persist Zustand store in localStorage to maintain state across sessions

--- client/src/store/user/public-user.store.js
+++ client/src/store/user/public-user.store.js
@@ -1,44 +1,49 @@
 import { create } from "zustand";
 import PublicUserService from "../../service/user/public-user.service.js";
 import { toast } from "react-toastify";
+import { persist } from "zustand/middleware";
 
-const usePublicUserStore = create((set, get) => ({
-  entity: undefined,
-  draft: undefined,
-  getEntity: () => {
-    return get().entity
-  },
-  getDraft: () => {
-    return get().draft
-  },
-  fetchPublicUser: async (userid) => {
-    const publicUser = await PublicUserService.getPubicUser(userid)
-    set((state) => ({ ...state, entity: publicUser, draft: publicUser }))
-  },
-  updatePublicUser: async (id, publicUser) => {
-    const updatedPublicUser = await PublicUserService.updatePublicUser(id, publicUser)
-    set((state) => ({ ...state, entity: updatedPublicUser, draft: updatedPublicUser }))
-  },
-  updateDraftPublicUser: async (publicUser) => {
-    set((state) => ({ ...state, draft: publicUser }))
-  },
-  saveDraft: async () => {
-    const draft = get().draft
-    if (draft) {
-      PublicUserService.updatePublicUser(draft)
-        .then(async () => {
-          toast.success("Account update successfully")
-          const publicUser = await PublicUserService.getPubicUser(draft.userid)
-          set((state) => ({ ...state, entity: publicUser, draft: publicUser }))
-        })
+const usePublicUserStore = create(persist((set, get) => ({
+    entity: undefined,
+    draft: undefined,
+    getEntity: () => {
+      return get().entity
+    },
+    getDraft: () => {
+      return get().draft
+    },
+    fetchPublicUser: async (userid) => {
+      const publicUser = await PublicUserService.getPubicUser(userid)
+      set((state) => ({ ...state, entity: publicUser, draft: publicUser }))
+    },
+    updatePublicUser: async (publicUser) => {
+      const updatedPublicUser = await PublicUserService.updatePublicUser(publicUser)
+      set((state) => ({ ...state, entity: updatedPublicUser, draft: updatedPublicUser }))
+    },
+    updateDraftPublicUser: async (publicUser) => {
+      set((state) => ({ ...state, draft: publicUser }))
+    },
+    saveDraft: async () => {
+      const draft = get().draft
+      if (draft) {
+        PublicUserService.updatePublicUser(draft)
+          .then(async () => {
+            toast.success("Account update successfully")
+            const publicUser = await PublicUserService.getPubicUser(draft.userid)
+            set((state) => ({ ...state, entity: publicUser, draft: publicUser }))
+          })
+      }
+    },
+    resetDraft: async () => {
+      const entity = get().entity
+      if (entity) {
+        set((state) => ({ ...state, draft: entity }))
+      }
     }
-  },
-  resetDraft: async () => {
-    const entity = get().entity
-    if (entity) {
-      set((state) => ({ ...state, draft: entity }))
-    }
+  }),
+  {
+    name: 'public-user-store'
   }
-}))
+))
 
 export default usePublicUserStore
