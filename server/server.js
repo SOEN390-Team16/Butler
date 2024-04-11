d1729e2fb454794dfba5d9b4f6c12e07bf599e14
ceyhuntopcu <ceyhuntopcu4107@gmail.com>
2024-04-11 19:29:28 -0400

    added the operation routes to server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ const employeeRoutes = require("./src/Employee/routes");
 const parkingSpotRoutes = require("./src/ParkingSpot/routes");
 const assignedParkingSpotRoutes = require("./src/assigned_ParkingSpot/routes");
 const RequestRoutes = require("./src/Request/routes");
-const facilityRoutes = require('./src/Facility/routes')
+const operationRoutes = require("./src/Operations/routes");
+const facilityRoutes = require("./src/Facility/routes");
 
 const cors = require("cors");
 
@@ -40,6 +41,7 @@ app.use("/api/v1/ps", parkingSpotRoutes);
 app.use("/api/v1/aps", assignedParkingSpotRoutes);
 app.use("/api/v1/emp", employeeRoutes);
 app.use("/api/v1/req", RequestRoutes);
-app.use('/api/v1/fa', facilityRoutes)
+app.use("/api/v1/op", operationRoutes);
+app.use("/api/v1/fa", facilityRoutes);
 
 app.listen(port, () => console.log(`app listening on ${port}`));
