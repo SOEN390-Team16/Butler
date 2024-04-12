2d852392e92f545f1a4ac4f881cf3d5a9f47882b
ceyhuntopcu <ceyhuntopcu4107@gmail.com>
2024-04-12 01:43:46 -0400

    added getReservationsByPropertyId

--- server/src/Reservation/queries.js
+++ server/src/Reservation/queries.js
@@ -2,6 +2,7 @@ const getAllReservations = 'SELECT * FROM reservation'
 const getAllReservationsByUserId = 'SELECT * FROM reservation WHERE user_id = $1'
 const getAllReservationsByDate = 'SELECT * FROM reservation WHERE date = $1'
 const getAllReservationsByFacilityId = 'SELECT * FROM reservation WHERE facility_id = $1'
+const getAllReservationsByPropertyId = 'SELECT res.reservation_id, res.user_id, res.facility_id, res.date FROM reservation res JOIN facility ON res.facility_id = facility.facility_id WHERE facility.property_id = $1';
 const getReservationById = 'SELECT * FROM reservation WHERE reservation_id = $1'
 const checkIfReservationExists = 'SELECT * FROM reservation WHERE reservation_id = $1'
 const checkIfReservationExistsByDetails = 'SELECT * FROM reservation WHERE user_id = $1 AND facility_id = $2 AND date = $3'
@@ -14,6 +15,7 @@ module.exports = {
   getAllReservationsByUserId,
   getAllReservationsByDate,
   getAllReservationsByFacilityId,
+  getAllReservationsByPropertyId,
   getReservationById,
   checkIfReservationExists,
   checkIfReservationExistsByDetails,
