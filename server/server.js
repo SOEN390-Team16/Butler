2b550e9d6a144ef7c8210c2b5a5b970118c21c02
Walid <walidoo_2002@hotmail.com>
2024-04-11 20:47:05 -0400

    feat(server): add endpoint to upload images

--- server/server.js
+++ server/server.js
@@ -14,9 +14,9 @@ const employeeRoutes = require("./src/Employee/routes");
 const parkingSpotRoutes = require("./src/ParkingSpot/routes");
 const assignedParkingSpotRoutes = require("./src/assigned_ParkingSpot/routes");
 const RequestRoutes = require("./src/Request/routes");
-const operationRoutes = require("./src/Operations/routes");
 const facilityRoutes = require("./src/Facility/routes");
 const reservationRoutes = require("./src/Reservation/routes");
+const imageRoutes = require("./src/image/routes");
 
 const cors = require("cors");
 
@@ -42,8 +42,8 @@ app.use("/api/v1/ps", parkingSpotRoutes);
 app.use("/api/v1/aps", assignedParkingSpotRoutes);
 app.use("/api/v1/emp", employeeRoutes);
 app.use("/api/v1/req", RequestRoutes);
-app.use("/api/v1/op", operationRoutes);
 app.use("/api/v1/fa", facilityRoutes);
 app.use("/api/v1/res", reservationRoutes);
+app.use("/api/v1/image", imageRoutes);
 
 app.listen(port, () => console.log(`app listening on ${port}`));
