18ef166aafa39737be5c6d65a011ec4ff08a172d
Walid <walidoo_2002@hotmail.com>
2024-04-12 00:18:27 -0400

    fix(client): profile picture is saved correctly

--- client/src/store/user/public-user.store.js
+++ client/src/store/user/public-user.store.js
@@ -1,6 +1,5 @@
 import { create } from "zustand";
 import PublicUserService from "../../service/user/public-user.service.js";
-import { toast } from "react-toastify";
 import { persist } from "zustand/middleware";
 
 const usePublicUserStore = create(persist((set, get) => ({
@@ -15,24 +14,28 @@ const usePublicUserStore = create(persist((set, get) => ({
     fetchPublicUser: async (userid) => {
       const publicUser = await PublicUserService.getPubicUser(userid)
       set((state) => ({ ...state, entity: publicUser, draft: publicUser }))
+      return true
     },
     updatePublicUser: async (publicUser) => {
       const updatedPublicUser = await PublicUserService.updatePublicUser(publicUser)
       set((state) => ({ ...state, entity: updatedPublicUser, draft: updatedPublicUser }))
+      return true
     },
     updateDraftPublicUser: async (publicUser) => {
       set((state) => ({ ...state, draft: publicUser }))
     },
     saveDraft: async () => {
+      let status = undefined
       const draft = get().draft
       if (draft) {
         PublicUserService.updatePublicUser(draft)
           .then(async () => {
-            toast.success("Account update successfully")
             const publicUser = await PublicUserService.getPubicUser(draft.userid)
             set((state) => ({ ...state, entity: publicUser, draft: publicUser }))
+            status = true
           })
       }
+      return status
     },
     resetDraft: async () => {
       const entity = get().entity
