bb9f0bae16e00b9f61e0a1727d308155ebcb809a
Houssam Righi <hrighi01@gmail.com>
2024-04-14 22:17:12 -0400

    Refactor reservation routes for consistency and readability

--- server/src/Reservation/routes.js
+++ server/src/Reservation/routes.js
@@ -1,16 +1,41 @@
-const { Router } = require('express')
-const controller = require('./controller')
-const authenticateToken = require('../auth/tokenValidator')
-const router = Router()
+const { Router } = require("express");
+const controller = require("./controller");
+const authenticateToken = require("../auth/tokenValidator");
+const router = Router();
 
-router.get('/', authenticateToken, controller.getAllReservations)
-router.get('/user/:userid', authenticateToken, controller.getReservationsByUserId)
-router.get('/date/:date', authenticateToken, controller.getReservationsByDate)
-router.get('/facility/:facilityid', authenticateToken, controller.getReservationsByFacilityId)
-router.get('/property/:propertyid', authenticateToken, controller.getReservationsByPropertyId)
-router.get('/:reservationid', authenticateToken, controller.getReservationById)
-router.post('/', authenticateToken, controller.createReservation)
-router.delete('/:reservationid', authenticateToken, controller.deleteReservation)
-router.patch('/:reservationid', authenticateToken, controller.updateReservation)
+router.get("/", authenticateToken, controller.getAllReservations);
+router.get(
+  "/user/:userid",
+  authenticateToken,
+  controller.getReservationsByUserId
+);
+router.get("/date/:date", authenticateToken, controller.getReservationsByDate);
+router.get(
+  "/facility/:facilityid",
+  authenticateToken,
+  controller.getReservationsByFacilityId
+);
+router.get(
+  "/property/:propertyid",
+  authenticateToken,
+  controller.getReservationsByPropertyId
+);
+router.get("/:reservationid", authenticateToken, controller.getReservationById);
+router.get(
+  "/status/:facilityid",
+  authenticateToken,
+  controller.getFacilityStatus
+);
+router.post("/", authenticateToken, controller.createReservation);
+router.delete(
+  "/:reservationid",
+  authenticateToken,
+  controller.deleteReservation
+);
+router.patch(
+  "/:reservationid",
+  authenticateToken,
+  controller.updateReservation
+);
 
-module.exports = router
+module.exports = router;
