8988482b8028b825c541441ef1ad228c1b7fd1c5
DanDuguay <Duguay9@gmail.com>
2024-04-15 22:31:08 -0400

    refactored CMC controller file to handle an optional property_id query param on get

--- server/src/CMC/queries.js
+++ server/src/CMC/queries.js
@@ -3,11 +3,18 @@ const getCMCById = 'SELECT companyID, company_name, email, password, role FROM c
 const checkIfCMCEmailExists = 'SELECT * FROM condo_management_company cmc WHERE cmc.email = $1;'
 const addCMC = 'INSERT INTO condo_management_company(company_name, email, password) VALUES ($1, $2, $3);'
 const removeCMC = 'DELETE FROM condo_management_company WHERE companyID = $1;'
+const checkIfPropertyExists = 'SELECT * FROM property WHERE property_id = $1;'
+const getCMCByPropertyId = 'SELECT cmc.*\n' +
+    'FROM condo_management_company cmc\n' +
+    'INNER JOIN property p ON cmc.companyid = p.companyid\n' +
+    'WHERE p.property_id = $1'
 
 module.exports = {
   getCMCs,
   getCMCById,
   checkIfCMCEmailExists,
   addCMC,
-  removeCMC
+  removeCMC,
+  checkIfPropertyExists,
+  getCMCByPropertyId
 }
