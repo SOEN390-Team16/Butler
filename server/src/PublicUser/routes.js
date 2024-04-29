8d6726083459148af6eb1b0cf22a7778bb3b2dcb
ceyhuntopcu <ceyhuntopcu4107@gmail.com>
2024-04-29 16:46:53 -0400

    Add Google sign-up and callback routes to PublicUser routes

--- server/src/PublicUser/routes.js
+++ server/src/PublicUser/routes.js
@@ -8,5 +8,7 @@ router.get('/:userid', authenticateToken, controller.getPublicUserById)
 router.post('/', controller.addPublicUser)
 router.patch('/:userid', authenticateToken, controller.updatePublicUser)
 router.delete('/:userid', authenticateToken, controller.removePublicUser)
+router.get('/google/signup', controller.googleSignUp)
+router.get('/google/signup/callback', controller.googleSignUpCallback)
 
 module.exports = router
