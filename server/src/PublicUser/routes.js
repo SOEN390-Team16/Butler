c27448df7807de50e95d1f17be84724516464c39
ceyhuntopcu <ceyhuntopcu4107@gmail.com>
2024-04-29 17:29:07 -0400

    Refactor GoogleAuth routes and controller

--- server/src/PublicUser/routes.js
+++ server/src/PublicUser/routes.js
@@ -8,7 +8,5 @@ router.get('/:userid', authenticateToken, controller.getPublicUserById)
 router.post('/', controller.addPublicUser)
 router.patch('/:userid', authenticateToken, controller.updatePublicUser)
 router.delete('/:userid', authenticateToken, controller.removePublicUser)
-router.get('/google/signup', controller.googleSignUp)
-router.get('/google/signup/callback', controller.googleSignUpCallback)
 
 module.exports = router
