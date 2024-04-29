da652f906da35bc008d9dde2661d744c17b143ce
ceyhuntopcu <ceyhuntopcu4107@gmail.com>
2024-04-29 17:30:44 -0400

    Add getPublicUserByEmail query to PublicUser queries

--- server/src/PublicUser/queries.js
+++ server/src/PublicUser/queries.js
@@ -4,11 +4,13 @@ const getPublicUserById = 'SELECT pu.userid, pu.first_name, pu.last_name, pu.ema
 const checkIfPUEmailExists = 'SELECT * FROM public_user pu WHERE pu.email = $1'
 const addPublicUser = 'INSERT INTO public_user(first_name, last_name, email, password) VALUES ($1, $2, $3, $4)'
 const removePublicUser = 'DELETE FROM public_user WHERE userid = $1 '
+const getPublicUserByEmail = 'SELECT * FROM public_user WHERE email = $1'
 
 module.exports = {
   getPublicUsers,
   getPublicUserById,
   checkIfPUEmailExists,
   addPublicUser,
-  removePublicUser
+  removePublicUser,
+  getPublicUserByEmail
 }
