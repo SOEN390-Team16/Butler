5674143ac779e07df2fb18e33574c7291e11ff33
DanDuguay <72354083+DanDuguay@users.noreply.github.com>
2024-04-30 02:13:17 -0400

    getCondoOwners and getCondoOwnerById now returns userid as well, same for getCondoRenters and getCondoRenterById (#277)

--- server/src/CondoOwner/queries.js
+++ server/src/CondoOwner/queries.js
@@ -1,9 +1,9 @@
 const getCondoOwners =
   'SELECT pu.first_name, pu.last_name, pu.email, pu.password, pu.profile_picture, pu.role, ' +
-  'co.ownerid FROM public_user pu, condo_owner co WHERE pu.userID = co.userID;'
+  'co.ownerid, pu.userid FROM public_user pu, condo_owner co WHERE pu.userID = co.userID;'
 const getCondoOwnerById =
   'SELECT pu.first_name, pu.last_name, pu.email, pu.password, pu.profile_picture, pu.role,' +
-  'co.ownerid FROM public_user pu JOIN condo_owner co ON pu.userid = co.userid WHERE co.ownerid = $1;'
+  'co.ownerid, pu.userid FROM public_user pu JOIN condo_owner co ON pu.userid = co.userid WHERE co.ownerid = $1;'
 const checkIfCOEmailExists = 'SELECT * FROM public_user pu WHERE pu.email = $1;'
 const checkIfUserAlreadyExists =
   "SELECT pu.role = 'condo_owner' OR pu.role = 'renter' FROM public_user pu WHERE pu.email = $1;"
