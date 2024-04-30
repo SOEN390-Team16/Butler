b2fe6587f857d50f199a41bc5bce89afb1602f56
DanDuguay <Duguay9@gmail.com>
2024-04-29 21:27:52 -0400

    added an endpoint to assing a condo unit to a user by passing a property_id and a user id. It will assign a random, not previously assigned condo unit from that property and change it's occupant_type column appropriately

--- server/src/CondoUnit/routes.js
+++ server/src/CondoUnit/routes.js
@@ -5,10 +5,12 @@ const router = Router()
 
 router.get('/', authenticateToken, controller.getCondoUnits)
 router.get('/:condoid', authenticateToken, controller.getCondoUnitById)
+router.get('/getByU/:userid', authenticateToken, controller.getCondoUnitByUserId)
 router.get('/:condoid/fees', authenticateToken, controller.calculateTotalCondoFee)
 router.post('/', authenticateToken, controller.addCondoUnit)
 router.post('/:condoid', authenticateToken, controller.calculateTotalCondoFee)
 router.delete('/:condoid', authenticateToken, controller.removeCondoUnit)
 router.patch('/:condoid', authenticateToken, controller.updateCondoUnit)
+router.patch('/assign/:property_id/:userid', authenticateToken, controller.assignCondoUnitToUser)
 
 module.exports = router
