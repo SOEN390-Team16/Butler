fa0624b636097265a8c134193266bf18ffd41457
yousfino <117748638+yousfino@users.noreply.github.com>
2024-04-30 21:26:32 -0400

    implement the following: - get all condo units for a specific company (cmc account) and display them in a table. - add new condo unit to a specific property. new condo is unassigned by default. - get all condo owners and display them in a table. - get all condo renter and display them in a table. - implement assign condo functionality to assign an unassigned condo to condo owner or a condo renter. - create Add Condo form (new component) to add new condos to a specific property using property id.

--- client/src/Components/Buttons/RegisterButton.jsx
+++ client/src/Components/Buttons/RegisterButton.jsx
@@ -2,16 +2,29 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 export default function RegisterButton(props) {
-  return (
-    <Link
-      className="flex flex-row items-center text-white font-bold text-inter bg-[#34A853] hover:bg-green-600
-      px-4 py-2.5 rounded-[5px] w-fit border-none text-base"
-      onClick={props.onClick}
-      to={props.to}
-    >
-      {props.children}
-    </Link>
-  );
+  if (props.to) {
+    return (
+      <Link
+        className="flex flex-row items-center text-white font-bold text-inter bg-[#34A853] hover:bg-green-600
+        px-4 py-2.5 rounded-[5px] w-fit border-none text-base"
+        onClick={props.onClick}
+        to={props.to}
+      >
+        {props.children}
+      </Link>
+    );
+  } else {
+    // Render as button if no navigation is needed
+    return (
+      <button
+        className="flex flex-row items-center text-white font-bold text-inter bg-[#34A853] hover:bg-green-600
+        px-4 py-2.5 rounded-[5px] w-fit border-none text-base"
+        onClick={props.onClick}
+      >
+        {props.children}
+      </button>
+    );
+  }
 }
 
 RegisterButton.propTypes = {
---
+++ client/src/Components/Dashboard/CondoAddForm.jsx
@@ -0,0 +1,90 @@
+import AddButton from "../Buttons/AddButton.jsx";
+import Input from "../Forms/Input.jsx";
+import { useFormik } from "formik";
+import Label from "../Forms/Label.jsx";
+import { useModal } from "../Modals/Modal.jsx";
+import { number, object } from 'yup';
+import axios from "axios";
+import { toast } from "react-toastify";
+import PropTypes from "prop-types";
+
+export default function CondoAddForm(props) {
+  const {toggle} = useModal();
+
+  let condoSchema = object({
+    condoNumber: number()
+      .typeError('Condo number must be a number')
+      .required('Condo number is required')
+      .integer('Condo number must be an integer')
+      .min(1, 'You must indicate a minimum of 1'),
+    condoSize: number()
+      .typeError('Condo size must be a number')
+      .required('Condo size is required')
+      .integer('Condo size must be an integer')
+      .min(100, 'You must indicate a minimum size of 100'),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      condoNumber: '', condoSize: '',
+    }, validationSchema: condoSchema, onSubmit: values => handleSubmit(values),
+  });
+
+  const errorMessage = (fieldName) => {
+    if (formik.touched[fieldName] && formik.errors[fieldName]) {
+      return <span className='text-red-400 text-sm'>{formik.errors[fieldName]}</span>
+    }
+    return null;
+  }
+
+  const handleSubmit = (values) => {
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    const storedPropertyId = localStorage.getItem('propertyId');
+    const propertyId = storedPropertyId ? parseInt(storedPropertyId, 10) : null;
+    const token = localStorage.getItem('token')
+    console.log("company id:", userData.cmcId);
+    console.log("property id:", propertyId);
+    const condo = {
+      companyid: userData.cmcId,
+      condo_number: values.condoNumber,
+      size: values.condoSize,
+      property_id: propertyId,
+    }
+
+    axios
+      .post("http://hortzcloud.com:3000/api/v1/cu/", condo, {
+        headers: {
+          'authorization': `Bearer ${token}`,
+        }
+      })
+      .then(() => {
+        toast.success('Condo Successfully Added!');
+        props.onAddCondo(condo)
+        toggle()
+      })
+      .catch((reason) => {
+        toast.error(`Something went wrong: ${reason.message}`)
+        throw reason
+      })
+  }
+
+  return (<>
+    <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
+      <div className="flex flex-col gap-2 w-[360px] font-inter h-fit">
+        <Label htmlFor="condoNumber">Condo Number</Label>
+        {errorMessage("condoNumber")}
+        <Input onChange={formik.handleChange} id="condoNumber" name="condoNumber"/>
+      </div>
+      <div className="flex flex-col gap-2 w-[360px] font-inter h-fit">
+        <Label htmlFor="condoSize">Condo Size</Label>
+        {errorMessage("condoSize")}
+        <Input onChange={formik.handleChange} id="condoSize" name="condoSize"/>
+      </div>
+    </form>
+    <AddButton onClick={formik.submitForm}>Add Condo</AddButton>
+  </>)
+}
+
+CondoAddForm.propTypes = {
+  onAddCondo: PropTypes.func.isRequired,
+}
