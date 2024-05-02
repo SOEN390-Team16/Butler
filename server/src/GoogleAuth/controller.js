6dd2be07ad31f298c3287a38bde1a3218115bca3
Houssam Righi <hrighi01@gmail.com>
2024-05-01 20:23:36 -0400

    Refactor GoogleAuth routes to use consistent naming conventions

--- server/src/GoogleAuth/controller.js
+++ server/src/GoogleAuth/controller.js
@@ -8,6 +8,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 const jwtSecretKey = process.env.JWT_SECRET_KEY
 const jwtRefreshKey = process.env.REFRESH_TOKEN_SECRET
 
+const CLIENT_URL = 'http://hortzcloud.com:8080'
+
 const generateTokens = (payload) => {
   const accessToken = jwt.sign(payload, jwtSecretKey, { expiresIn: '30m' })
   const refreshToken = jwt.sign(payload, jwtRefreshKey)
@@ -22,26 +24,65 @@ passport.use(new GoogleStrategy({
   done(null, profile)
 }))
 
-const googleSignUp = passport.authenticate('google', { scope: ['profile', 'email'] })
+const googleAuth = passport.authenticate('google', { scope: ['profile', 'email'] })
 
-const googleSignUpCallback = (req, res, next) => {
+const googleAuthCallback = (req, res, next) => {
   passport.authenticate('google', async (err, profile) => {
     if (err) {
       return res.status(500).json({ error: 'Internal Server Error' })
     }
+
     // Check if the user already exists in your database based on Google profile data
     const email = profile.emails[0].value
     try {
       const userExists = await pool.query(queries.checkIfPUEmailExists, [email])
+
+      // If user exists, generate tokens and redirect to appropriate dashboard
       if (userExists.rows.length !== 0) {
-        return res.status(409).json({ error: 'User already exists' })
+        // Get user information
+        const userData = userExists.rows[0]
+        const token = generateTokens({
+          userid: userData.userid,
+          first_name: userData.first_name,
+          last_name: userData.last_name,
+          email: userData.email,
+          role: userData.role,
+          profile_picture: userData.profile_picture
+        })
+
+        switch (String(userData.role)) {
+          case 'condo_owner':
+            return res.redirect(`${CLIENT_URL}/DashBoardHomeCO?token=${token.accessToken}`)
+          case 'renter':
+            return res.redirect(`${CLIENT_URL}/DashBoardHomeCR?token=${token.accessToken}`)
+          case 'public_user':
+            return res.redirect(`${CLIENT_URL}/DashboardHome/editUser?token=${token.accessToken}`)
+          default:
+            return res.status(404).json({ error: 'Error fetching user role' })
+        }
       } else {
         const hashedPassword = await bcrypt.hash(profile.id, 5) // Using Google profile ID as password
         const { displayName } = profile
         const firstName = displayName.split(' ')[0]
         const lastName = displayName.split(' ')[1]
+
+        // Add user to database
         await pool.query(queries.addPublicUser, [firstName, lastName, email, hashedPassword])
-        return res.status(201).json({ message: 'User signed up successfully' })
+
+        // Get user information
+        const userData = await pool.query(queries.getPublicUserByEmail, [email])
+
+        // Generate tokens and redirect to public dashboard
+        const token = generateTokens({
+          userid: userData.userid,
+          first_name: userData.first_name,
+          last_lame: userData.last_name,
+          email: userData.email,
+          role: userData.role,
+          profile_picture: userData.profile_picture
+        })
+
+        return res.redirect(`${CLIENT_URL}/DashboardHome/editUser?token=${token.accessToken}`)
       }
     } catch (error) {
       console.error('Error signing up with Google:', error)
@@ -50,39 +91,7 @@ const googleSignUpCallback = (req, res, next) => {
   })(req, res, next)
 }
 
-const googleSignIn = passport.authenticate('google', { scope: ['profile', 'email'] })
-
-const googleSignInCallback = (req, res, next) => {
-  passport.authenticate('google', async (err, profile) => {
-    if (err) {
-      return res.status(500).json({ error: 'Internal Server Error' })
-    }
-    // Check if the user already exists in your database based on Google profile data
-    const email = profile.emails[0].value
-    try {
-      const user = await pool.query(queries.getPublicUserByEmail, [email])
-      if (user.rows.length === 0) {
-        return res.status(404).json({ error: 'User not found' })
-      }
-      const userData = user.rows[0]
-      const token = generateTokens({
-        userId: userData.userid,
-        firstName: userData.first_name,
-        lastName: userData.last_name,
-        email: userData.email,
-        role: userData.role
-      })
-      return res.status(200).json({ message: 'User signed in successfully', token })
-    } catch (error) {
-      console.error('Error signing in with Google:', error)
-      return res.status(500).json({ error: 'Internal Server Error' })
-    }
-  })(req, res, next)
-}
-
 module.exports = {
-  googleSignUp,
-  googleSignUpCallback,
-  googleSignIn,
-  googleSignInCallback
+  googleAuth,
+  googleAuthCallback
 }
--- server/src/GoogleAuth/routes.js
+++ server/src/GoogleAuth/routes.js
@@ -4,7 +4,5 @@ const router = Router()
 
 router.get('/', controller.googleAuth)
 router.get('/callback', controller.googleAuthCallback)
-router.get('/signin', controller.googleSignIn)
-router.get('/login/callback', controller.googleSignInCallback)
 
 module.exports = router
